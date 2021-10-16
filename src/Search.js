99963aa3ac430a75cdf4f69dc39366cee508a3fe
ter-gav <86326060+ter-gav@users.noreply.github.com>
2021-10-16 18:18:27 +0200

    added form

--- src/Search.js
+++ src/Search.js
@@ -2,10 +2,16 @@ import React from "react"
 
 export default function Search() {
     return <div className="Search">
+        
         <form>
+        <div className="row">
+        <div className="col-9">
             <input type="search" placeholder="Enter a city"></input>
-
+            </div>
+            <div className="col-3">
+            <input type="submit" value="Search"></input>
+        </div>
+        </div>
         </form>
-        <button className="btn btn-primary">Search</button>
     </div>
 }
