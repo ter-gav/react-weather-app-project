2eabcaeee7ee7433c748b661b88aa400537cc201
ter-gav <86326060+ter-gav@users.noreply.github.com>
2021-10-16 14:53:59 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div className="App">
-    Weather App
+      <div className="container">
+    <h1>Weather App</h1>
+    </div>
     </div>
   );
 }
