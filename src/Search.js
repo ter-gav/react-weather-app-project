0ff0a978ed265d8bd2281b7305b7d932bba59fc0
ter-gav <86326060+ter-gav@users.noreply.github.com>
2021-10-19 21:15:40 +0200

    added css to form and Search component

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,17 @@
 import React from "react"
+import "./Search.css"
 
 export default function Search() {
     return <div className="Search">
         <form>
-            <input type="search" placeholder="Enter a city"></input>
-            <input type="submit" value="Search"></input>
+            <div className="row">
+                <div className="col-9">
+            <input type="search" placeholder="Enter a city" className="form-control"></input>
+            </div>
+            <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary w-100"></input>
+            </div>
+            </div>
         </form>
     </div>
 }
