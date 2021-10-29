4adb3e60671ece266df3b0e06fc9f37b04405a5a
ter-gav <86326060+ter-gav@users.noreply.github.com>
2021-10-29 16:36:07 +0200

    added links to repository and netlify

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ export default function App() {
       <div className="container">
         <WeatherSearch defaultCity="Berlin" />
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://flamboyant-blackwell-9acb6f.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Teresa Gavric
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ter-gav/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://pensive-hypatia-d78523.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
