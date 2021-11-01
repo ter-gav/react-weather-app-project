1d43c7513676646544d4e851a0ed4795b94f5efe
ter-gav <86326060+ter-gav@users.noreply.github.com>
2021-11-01 22:46:39 +0100

    refactored code and addes css to celsius

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,46 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
+
+import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div>
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="units">
-          {" "}
-          ºC |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            ºF
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <span className="temperature">{Math.round(fahrenheit())}</span>
-        <span className="units">
-          {" "}
-          <a href="/" onClick={showCelsius}>
-            ºC
-          </a>{" "}
-          | ºF
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="units"> ºC</span>
+    </div>
+  );
 }
